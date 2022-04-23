2e1ea42b51e30c949d705ae88218498908a97b8c
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2022-04-23 23:32:46 +0700

    menambahkan API Movies

--- modul8_1302200010/Controllers/MovieController.cs
+++ modul8_1302200010/Controllers/MovieController.cs
@@ -8,36 +8,38 @@ namespace modul8_1302200010.Controllers
     [ApiController]
     public class MovieController : ControllerBase
     {
-        // GET: api/<MovieController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private static List<Movie> Movies = new List<Movie>()
         {
-            return new string[] { "value1", "value2" };
+            new Movie("The Shawshank Redemption", "Frank Darabont", new List<string>(){"Tim Robbins", "Morgan Freeman", "Bob Gunton"}, "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."),
+            new Movie("The Godfather", "Francis Ford Coppola", new List<string>(){ "Marlon Brando", "Al Pacino", "James Caan"}, "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."),
+            new Movie("The Dark Knight", "Christopher Nolan", new List<string>(){ "Christian Bale", "Heath Ledger", "Aaron Eckhart"}, "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."),
+        };
+        // GET: api/Movies
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return Movies;
         }
 
-        // GET api/<MovieController>/5
+        // GET api/Movies/{id}
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Movie Get(int id)
         {
-            return "value";
+            return Movies[id];
         }
 
         // POST api/<MovieController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Movie value)
         {
-        }
-
-        // PUT api/<MovieController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+            Movies.Add(value); 
         }
 
         // DELETE api/<MovieController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            Movies.RemoveAt(id);
         }
     }
 }
